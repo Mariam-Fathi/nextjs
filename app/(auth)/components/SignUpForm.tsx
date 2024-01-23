2aa8b6dba1c0a1c8f6ae3c40a7e9d46d16cbbd56
Mariam Fathi <66276072+Mariam-Fathi@users.noreply.github.com>
2024-01-23 22:53:01 +0200

    Update SignUpForm.tsx

--- app/(auth)/components/SignUpForm.tsx
+++ app/(auth)/components/SignUpForm.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { TbBracketsAngle } from "react-icons/tb";
 import Input from "@/components/ui/Input";
 import { useState } from "react";
 import Button from "@/components/ui/Button";
@@ -44,7 +43,6 @@ const SignUpForm = () => {
       <div className="px-4 py-8 sm:rounded-lg sm:px-10">
         <div className="md:text-4xl sm:text-2xl mb-5 uppercase w-full text-center flex items-center text-white gap-1 justify-center bg-gray-700 py-4 rounded-md">
           <h1>Join the HANDBAG</h1>
-          <TbBracketsAngle />
         </div>
 
         <form
